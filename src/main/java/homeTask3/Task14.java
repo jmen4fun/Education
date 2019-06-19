a0a4713a4059762f56a76f58bc0b58f4d7520a1b
jmen4fun <jmen4fun@gmail.com>
2019-06-19 22:10:29 +0300

    v4

--- src/main/java/homeTask3/Task14.java
+++ src/main/java/homeTask3/Task14.java
@@ -1,25 +1,22 @@
 package homeTask3;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task14 {
+
     public static void main(String[] args) {
         int a, b, c;
-
         System.out.println("Enter a, b, c: ");
         Scanner num = new Scanner(System.in);
         a = num.nextInt();
         b = num.nextInt();
         c = num.nextInt();
-
-        if (b > a && a > c || c > a && a > b){
-            System.out.println("Value " + a + " is first");
-        }else if (a > b && b > c || c > b && b > a){
-            System.out.println("Value " + b + " is second");
-        }else if (a > c && c > b || b > c && c > a){
-            System.out.println("Value " + c + " is third");
-        }else if (a == b || a == c || b == c) {
-            System.out.println("Numbers are the same");
-        }
+        int[] result = {a, b, c};
+        Arrays.sort(result);
+        System.out.println("Value " + result[0] + " is first");
+        System.out.println("Value " + result[1] + " is second");
+        System.out.println("Value " + result[2] + " is second");
     }
 }
