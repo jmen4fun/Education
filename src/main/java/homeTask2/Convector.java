6e9aae8e0ec33d7961f785855f6f708d88a78a46
jmen4fun <jmen4fun@gmail.com>
2019-07-01 23:52:40 +0300

    v4

--- src/main/java/homeTask2/Convector.java
+++ src/main/java/homeTask2/Convector.java
@@ -4,57 +4,90 @@ import java.util.Scanner;
 
 public class Convector {
 
-    public static void main(String... args) {
+    public static final double INCHES = 39.37;
+    public static final double KILOMETERS = 39.37;
+    public static final double POUNDS = 39.37;
+    public static final double KM_TO_MI = 39.37;
+    public static final double KELVIN = 273.15;
+    public static final double FAHRENHEIT_VALUE = 9f / 5;
+    public static final double FAHRENHEIT_VALUE_2 = 32;
+    public static final double FAHRENHEIT_VALUE_3 = 5f / 9;
 
-        double inches = 39.37;
-        double kilometres = 1.30934;
-        double pounds = 2.205;
-        double kmTOmi = 1.609;
+    public static void main(String... args) {
 
         System.out.println("Wellcome to Сonvector:");
         System.out.println("Select:\n 1. Meters to Inches\n 2. Miles to Kilometres\n " +
                 "3. Kilograms to Pounds\n 4. Speed: km/hr to mi/hr\n 5. Celsius to Fahrenheit\n " +
                 "6. Fahrenheit to Celsius\n 7. Celsius to Kelvin");
 
-        int a = Integer.parseInt(new Scanner(System.in).nextLine());
+        int convectorType = Integer.parseInt(new Scanner(System.in).nextLine());
 
         System.out.println("Enter value:");
 
-        double b = Double.parseDouble(new Scanner(System.in).nextLine());
+        double value = Double.parseDouble(new Scanner(System.in).nextLine());
         double result = 0;
 
-        switch (a){
-            case(1):
-                result = b * inches;
+        switch (convectorType) {
+            case (1):
+                result = metersToinches(value);
                 break;
 
-            case(2):
-                result = b * kilometres;
+            case (2):
+                result = milesTokilometres(value);
                 break;
 
-            case(3):
-                result = b * pounds;
+            case (3):
+                result = kilogramsTopounds(value);
                 break;
 
-            case(4):
-                result = b / kmTOmi;
+            case (4):
+                result = speeds(value);
                 break;
 
-            case(5):
-                result = (b * 9/5) + 32;
+            case (5):
+                result = celsiusTofahrenheit(value);
                 break;
 
-            case(6):
-                result = (b - 32) * 5/9;
+            case (6):
+                result = fahrenheitTocelsius(value);
                 break;
 
-            case(7):
-                result = b + 273.15;
+            case (7):
+                result = kelvins(value);
                 break;
 
-                default:
-                    System.out.println("Nice try");
+            default:
+                System.out.println("Nice try");
         }
         System.out.println(result);
+
+    }
+
+    public static double metersToinches(double metr) {
+        return metr * INCHES;
+    }
+
+    public static double milesTokilometres(double miles) {
+        return miles * KILOMETERS;
+    }
+
+    public static double kilogramsTopounds(double pound) {
+        return pound * POUNDS;
+    }
+
+    public static double speeds(double speed) {
+        return speed * KM_TO_MI;
+    }
+
+    public static double celsiusTofahrenheit(double fahrent) {
+        return (fahrent * FAHRENHEIT_VALUE) + FAHRENHEIT_VALUE_2;
+    }
+
+    public static double fahrenheitTocelsius(double celsius) {
+        return (celsius - FAHRENHEIT_VALUE_2) * FAHRENHEIT_VALUE_3;
+    }
+
+    public static double kelvins(double kelvin) {
+        return kelvin + KELVIN;
     }
 }
